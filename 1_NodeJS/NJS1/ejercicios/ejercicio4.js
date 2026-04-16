f7548b76b151ab3e738129a71f996d519f9ed142
Leonel Suarez <leonelsuarezmiranda@gmail.com>
2026-04-16 19:58:34 -0300

    Proyecto completo ordenado

---
+++ 1_NodeJS/NJS1/ejercicios/ejercicio4.js
@@ -0,0 +1,6 @@
+import { sumar, restar, multiplicar, dividir } from './calculos.js';
+
+console.log("Suma:", sumar(5, 3));
+console.log("Resta:", restar(8, 6));
+console.log("Multiplicación:", multiplicar(3, 11));
+console.log("División:", dividir(30, 5));
